7ca222316e749c94fb97bdff675abf385e79e226
Self-denial <731813856@qq.com>
2016-07-26 21:49:20 +0800

    bottomfragment下面体颜色，创建了个人主页PersonalActivity，增加了欢迎页面。

---
+++ app/src/main/java/com/one/duanone/fragment/CenterFragment.java
@@ -0,0 +1,13 @@
+package com.one.duanone.fragment;
+
+import android.view.View;
+
+/**
+ * Created by jj on 2016/7/26.
+ */
+public abstract class CenterFragment extends BaseFragment{
+
+    public abstract View getLeftView();
+    public abstract View getRightView();
+    public abstract View getCenterView();
+}
